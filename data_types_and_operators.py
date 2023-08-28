0f80dfbc695c4e0c37302f5ac03133b2aa604b49
omotomiwa26 <omotomiwa26@gmail.com>
2023-08-29 00:22:13 +0100

    data types and operators quiz practise

---
+++ data_types_and_operators.py
@@ -0,0 +1,16 @@
+# Write an expression that calculates the average of 23, 32 and 64
+# Place the expression in this print statement
+sum = 23 + 32 + 64
+avg = sum / 3
+print(avg)
+
+# Fill this in with an expression that calculates how many tiles are needed.
+first_floor = 9 * 7
+second_floor = 5 * 7
+total_tiles_needed = first_floor + second_floor
+print("total_tiles_needed is" + str(total_tiles_needed))
+
+total_tiles_bought = 17 * 6
+tiles_left = total_tiles_bought - total_tiles_needed
+# Fill this in with an expression that calculates how many tiles will be left over.
+print("tiles_left is" + str(tiles_left))
