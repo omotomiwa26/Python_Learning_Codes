e10b7aa057d093bd6b78032e76e1e3074b593383
omotomiwa26 <omotomiwa26@gmail.com>
2023-08-30 06:17:46 +0100

    string quiz 4 solved

--- strings.py
+++ strings.py
@@ -24,3 +24,15 @@ url = "http://petshop.com/pets/mammals/cats"
 # The message should have the same format as this one:
 # "Yogesh accessed the site http://petshop.com/pets/reptiles/pythons at 16:20."
 print (username + ' ''accessed the site' ' ' + url + ' ''at'' ' + timestamp + '.')
+
+## string quiz 4
+mon_sales = "121"
+tues_sales = "105"
+wed_sales = "110"
+thurs_sales = "98"
+fri_sales = "95"
+
+#TODO: Print a string with this format: This week's total sales: xxx
+# You will probably need to write some lines of code before the print statement.
+total_sales = int(mon_sales) + int(tues_sales) + int(wed_sales) + int(thurs_sales) + int(fri_sales)
+print ("This week's total sales: " + str(total_sales))
