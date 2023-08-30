f956e72e1e178c0487e4ab832e14071a0630619f
omotomiwa26 <omotomiwa26@gmail.com>
2023-08-30 05:52:15 +0100

    string quiz 3 solved

--- strings.py
+++ strings.py
@@ -14,3 +14,13 @@ name_length = len(given_name + ' ') + len(middle_names + ' ') + len(family_name)
 # Nothing you need to do here
 driving_license_character_limit = 28
 print(name_length <= driving_license_character_limit)
+
+## string quiz 3
+username = "Kinari"
+timestamp = "04:50"
+url = "http://petshop.com/pets/mammals/cats"
+
+# TODO: print a log message using the variables above.
+# The message should have the same format as this one:
+# "Yogesh accessed the site http://petshop.com/pets/reptiles/pythons at 16:20."
+print (username + ' ''accessed the site' ' ' + url + ' ''at'' ' + timestamp + '.')
