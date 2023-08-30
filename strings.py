ad0e396a9d8989066d1f46c810194478a35fe048
omotomiwa26 <omotomiwa26@gmail.com>
2023-08-30 05:40:55 +0100

    string quiz 2 solution

--- strings.py
+++ strings.py
@@ -1,3 +1,16 @@
 # TODO: Fix this string!
 ford_quote = 'Whether you think you can, or you think you can\'t--you\'re right.'
 print (ford_quote)
+
+
+## string quiz 2
+given_name = "William"
+middle_names = "Bradley"
+family_name = "Pitt"
+
+name_length = len(given_name + ' ') + len(middle_names + ' ') + len(family_name)
+
+# Now we check to make sure that the name fits within the driving license character limit
+# Nothing you need to do here
+driving_license_character_limit = 28
+print(name_length <= driving_license_character_limit)
