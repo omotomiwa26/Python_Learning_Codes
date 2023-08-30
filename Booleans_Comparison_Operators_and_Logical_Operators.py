dc058cdbe891116ff639062eb4ee23339e931c97
omotomiwa26 <omotomiwa26@gmail.com>
2023-08-30 04:34:06 +0100

    boolean quiz

--- Booleans_Comparison_Operators_and_Logical_Operators.py
+++ Booleans_Comparison_Operators_and_Logical_Operators.py
@@ -5,4 +5,12 @@ san_francisco_pop_density = sf_population/sf_area
 rio_de_janeiro_pop_density = rio_population/rio_area
 
 # Write code that prints True if San Francisco is denser than Rio, and False otherwise
+print (san_francisco_pop_density)
+print (rio_de_janeiro_pop_density)
 print (san_francisco_pop_density > rio_de_janeiro_pop_density)
+
+## alternative way
+if (san_francisco_pop_density > rio_de_janeiro_pop_density):
+ print (True)
+else:
+ (False)
