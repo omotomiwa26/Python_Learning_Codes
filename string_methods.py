399d9ba80a0b8c30360f9ac5deb5c4cd725090e6
omotomiwa26 <omotomiwa26@gmail.com>
2023-10-09 03:56:48 +0100

    String_methods.py

--- string_methods.py
+++ string_methods.py
@@ -11,3 +11,10 @@ print('Does your {} {}?'.format(animal, action))
 
 maria_string = 'Maria loves {} and {}'
 print(maria_string.format('math', 'statistics'))
+
+
+new_str = 'The cow jumoed over the moon.'
+new_str.split()
+new_str.split('', 3)
+new_str.split('.')
+new_str.split(None, 3)
