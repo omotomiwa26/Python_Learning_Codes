0ca67ea3f623cf0e96d2b5ec58295ee95196686b
omotomiwa26 <omotomiwa26@gmail.com>
2023-10-09 04:02:43 +0100

    string_methods

--- string_methods.py
+++ string_methods.py
@@ -13,8 +13,12 @@ maria_string = 'Maria loves {} and {}'
 print(maria_string.format('math', 'statistics'))
 
 
-new_str = 'The cow jumoed over the moon.'
-new_str.split()
-new_str.split('', 3)
-new_str.split('.')
-new_str.split(None, 3)
+str = 'The cow jumped over the moon.'
+new_str = str.split()
+print(new_str)
+new_str = str.split(' ', 3)
+print(new_str)
+new_str = str.split('.')
+print(new_str)
+new_str = str.split(None, 3)
+print(new_str)
