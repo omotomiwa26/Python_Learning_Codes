da890ad196bb4a3d284dd6065cfde0f63530e500
omotomiwa26 <omotomiwa26@gmail.com>
2023-10-09 03:47:46 +0100

    string_methods.py

---
+++ string_methods.py
@@ -0,0 +1,13 @@
+my_string = 'sebastian thrum'
+my_string.islower()
+my_string.count('a')
+my_string.find('a')
+
+print('Mohamed has {} ballons'.format(27))
+
+animal = 'dog' 
+action = 'bite'
+print('Does your {} {}?'.format(animal, action))
+
+maria_string = 'Maria loves {} and {}'
+print(maria_string.format('math', 'statistics'))
