5a02709399cf82e31b7d34c6ea0cedbaa38678e1
omotomiwa26 <omotomiwa26@gmail.com>
2023-10-14 00:19:47 +0100

    lists data type

---
+++ lists.py
@@ -0,0 +1,5 @@
+months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+print(months[0])
+print(months[7])
+print(months[-2])
+print(months[10])
