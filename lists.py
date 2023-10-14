7bf1daa5696ff66745c90d5efca1108366f9dd0d
omotomiwa26 <omotomiwa26@gmail.com>
2023-10-14 01:00:41 +0100

    lists

--- lists.py
+++ lists.py
@@ -8,3 +8,11 @@ print(len(months))
 print(months[6:9])
 print(months[:6])
 print(months[6:])
+
+month = 8
+days_in_month = [31,28,31,30,31,30,31,31,30,31,30,31]
+
+# use list indexing to determine the number of days in month
+num_days = days_in_month[9]
+
+print(num_days)
