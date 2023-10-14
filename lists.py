ec67ce9c96abd6c8b526d318b3695e943b3bbe70
omotomiwa26 <omotomiwa26@gmail.com>
2023-10-14 01:11:47 +0100

    lists

--- lists.py
+++ lists.py
@@ -16,3 +16,13 @@ days_in_month = [31,28,31,30,31,30,31,31,30,31,30,31]
 num_days = days_in_month[9]
 
 print(num_days)
+
+eclipse_dates = ['June 21, 2001', 'December 4, 2002', 'November 23, 2003',
+                 'March 29, 2006', 'August 1, 2008', 'July 22, 2009',
+                 'July 11, 2010', 'November 13, 2012', 'March 20, 2015',
+                 'March 9, 2016']
+                 
+                 
+# TODO: Modify this line so it prints the last three elements of the list
+eclipse_dates = eclipse_dates[7:]
+print(eclipse_dates)
