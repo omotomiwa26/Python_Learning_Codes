beff48568ab3892d67f65834642761c6af12b12a
omotomiwa26 <omotomiwa26@gmail.com>
2023-10-15 03:25:09 +0100

    tuples

---
+++ tuples.py
@@ -0,0 +1,8 @@
+AngorWat = (13.4125, 103.866667)
+print(type(AngorWat))
+print('AngorWat is a lattitude:{}'.format(AngorWat[0]))
+print('AngorWat is a longitude:{}'.format(AngorWat[1]))
+
+length, width, height = 52, 40, 100
+print('The dimensions are {} x {} x {}'.format(length, width, height))
+
