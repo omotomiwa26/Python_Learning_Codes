b9981b6496e02fb5cb7eaf18467e69f0e5490aca
omotomiwa26 <omotomiwa26@gmail.com>
2023-10-15 04:02:03 +0100

    dict

--- dictionaries.py
+++ dictionaries.py
@@ -8,3 +8,18 @@ n = elements.get('dilithium')
 print(n is None)
 print(n is not None)
 
+# Define a Dictionary, population,
+# that provides information
+# on the world's largest cities.
+# The key is the name of a city
+# (a string), and the associated
+# value is its population in
+# millions of people.
+
+#   Key     |   Value
+# Shanghai  |   17.8
+# Istanbul  |   13.3
+# Karachi   |   13.0
+# Mumbai    |   12.5
+
+population = {'Shanghai': 17.8, 'Istanbul': 13.3, 'Karachi': 13.0, 'Mumbai': 12.5}
