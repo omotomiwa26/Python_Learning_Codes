90b0955a7714e5d4183981b21873e84e3cbe72d8
omotomiwa26 <omotomiwa26@gmail.com>
2023-10-15 03:24:45 +0100

    lists

--- lists.py
+++ lists.py
@@ -1,7 +1,7 @@
 months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 print(months[0])
 print(months[7])
-print(months[-2])
+print(months[-1])
 print(months[10])
 print(len(months))
 
