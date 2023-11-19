bb27158eba6feff45d884bc7bde7a1c15e94fcc3
omotomiwa26 <omotomiwa26@gmail.com>
2023-11-19 01:15:49 +0100

    ticket price

--- conditional_statements.py
+++ conditional_statements.py
@@ -1,3 +1,4 @@
+#----------Bank banlace--------------#
 phone_balance = 5
 bank_balance = 50
 
@@ -8,9 +9,32 @@ if phone_balance < 10:
 print(phone_balance)
 print(bank_balance)
 
+
+#---------------Even or Odd numner check--------------#
 number = 14
 if number % 2 == 0:
     print("Number " +str(number) + " is even." )
 else:
     print("Number " + str(number) + " is odd. ")
+
+
+#----------------Ticket Price-----------------#
+age = 64
+
+free_up_to_age = 4
+child_up_to_age = 18
+senior_from_age = 65
+
+concession_ticket = 1.25
+adult_ticket = 2.50
+
+if age <= free_up_to_age:
+    ticket_price = 0
+elif age <= child_up_to_age:
+    ticket_price = concession_ticket
+elif age >= senior_from_age:
+    ticket_price = concession_ticket
+else:
+    ticket_price = adult_ticket
+print("Somebody who is {} years old will pay ${} to ride the bus".format(age, ticket_price))
 
