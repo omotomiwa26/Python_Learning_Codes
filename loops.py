65977e4cca5d077424a54690315c82bf2b9faf1e
omotomiwa26 <omotomiwa26@gmail.com>
2023-11-19 05:25:25 +0100

    create html list

--- loops.py
+++ loops.py
@@ -31,4 +31,12 @@ for token in tokens:
         count +=1
 print(count)
 
-#---------------
+#---------------create HTML list--------------#
+items = ['first string', 'second string']
+html_str = "<ul>\n"          # The "\n" here is the end-of-line char, causing
+                             # chars after this in html_str to be on next line
+for item in items:
+    html_str += '<li>{}</li>\n'.format(item)
+html_str += '</ul>'
+
+print(html_str)
