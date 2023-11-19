680a9cb3c761ac0d46e13df2c964c336d8592f02
omotomiwa26 <omotomiwa26@gmail.com>
2023-11-19 02:09:51 +0100

    guess number

--- conditional_statements.py
+++ conditional_statements.py
@@ -58,3 +58,28 @@ else:
     result = "Points not valid"
 
 print(result)
+
+#----------------Number Guess-----------#
+# '''
+# You decide you want to play a game where you are hiding 
+# a number from someone.  Store this number in a variable 
+# called 'answer'.  Another user provides a number called
+# 'guess'.  By comparing guess to answer, you inform the user
+# if their guess is too high or too low.
+
+# Fill in the conditionals below to inform the user about how
+# their guess compares to the answer.
+# '''
+answer = 190
+guess = 55
+
+if answer > guess:
+    result = "Oops!  Your guess was too low."
+elif answer < guess:
+    result = "Oops!  Your guess was too high."
+elif anwser == guess:
+    result = "Nice!  Your guess matched the answer!"
+
+print(result)
+
+#---------------
