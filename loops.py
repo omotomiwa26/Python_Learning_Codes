a3826bd11c499c000745da10fccd83d16769d75f
omotomiwa26 <omotomiwa26@gmail.com>
2023-11-19 05:42:30 +0100

    create html list

--- loops.py
+++ loops.py
@@ -35,8 +35,8 @@ print(count)
 items = ['first string', 'second string']
 html_str = "<ul>\n"          # The "\n" here is the end-of-line char, causing
                              # chars after this in html_str to be on next line
-for item in items:
-    html_str += '<li>{}</li>\n'.format(item)
+for index in range(len(items)):
+    html_str += F'<li>{items[index]}</li>\n'
 html_str += '</ul>'
 
 print(html_str)
