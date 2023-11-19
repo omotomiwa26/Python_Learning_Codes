a87e0740924defb16685b09c83ff2df41b8a95ab
omotomiwa26 <omotomiwa26@gmail.com>
2023-11-19 01:00:17 +0100

    odd or even

--- conditional_statements.py
+++ conditional_statements.py
@@ -7,3 +7,10 @@ if phone_balance < 10:
 
 print(phone_balance)
 print(bank_balance)
+
+number = 14
+if number % 2 == 0:
+    print("Number " +str(number) + " is even." )
+else:
+    print("Number " + str(number) + " is odd. ")
+
