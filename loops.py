110604f78d2f4b7dfa57488148ec720c47f6eba3
omotomiwa26 <omotomiwa26@gmail.com>
2023-11-19 04:15:01 +0100

    multiple of 5

--- loops.py
+++ loops.py
@@ -3,4 +3,7 @@ lists = ['the', 'quick', 'brown', 'fox', 'jumped', 'over', 'the', 'lazy', 'dog']
 
 for list in lists:
     print(list)
-    
+
+#------------nultiple of 5-----------------#
+for num in range(5, 31, 5):
+    print(num)
