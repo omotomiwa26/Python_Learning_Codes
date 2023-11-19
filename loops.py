66f56cc2c87cd7266139c6f75f4ee6550cacf3f2
omotomiwa26 <omotomiwa26@gmail.com>
2023-11-19 04:54:18 +0100

    create usernames

--- loops.py
+++ loops.py
@@ -7,3 +7,13 @@ for list in lists:
 #------------nultiple of 5-----------------#
 for num in range(5, 31, 5):
     print(num)
+
+#-------------name range-----------#
+names = ["Joey Tribbiani", "Monica Geller", "Chandler Bing", "Phoebe Buffay"]
+usernames = []
+
+# write your for loop here
+for index in range(len(names)):
+    names[index] = names[index].lower()
+    usernames.append(names[index].replace(" ", "_"))
+print(usernames)
