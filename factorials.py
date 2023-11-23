cc5a5b62474736994f43c81b2b187011cebe3165
omotomiwa26 <omotomiwa26@gmail.com>
2023-11-24 00:54:35 +0100

    factorial using while loop

--- factorials.py
+++ factorials.py
@@ -10,3 +10,17 @@ for n in range(1, number+1):
 print(product)
 
 #---------while loop factorail-------------#
+# number to find the factorial of
+number = 5   
+# start with our product equal to one
+product = 1
+# track the current number being multiplied
+current = 1
+# write your while loop here
+while number >= current:
+    #multiply the product so far by the current number
+    product *= current
+    # increment current with each iteration until it reaches number
+    current += 1
+# print the factorial of number
+print(product)
