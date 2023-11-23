49dcadd1f5eaf75b6a8067b0d8c7c5ae0cf0ad6e
omotomiwa26 <omotomiwa26@gmail.com>
2023-11-24 00:48:13 +0100

    factorial in for loop

---
+++ factorials.py
@@ -0,0 +1,12 @@
+#-----------for loop factorial----------#
+# number to find the factorial of
+number = 6 
+# start with our product equal to one
+product = 1
+# write your for loop here
+for n in range(1, number+1):
+    product *= n
+# print the factorial of number
+print(product)
+
+#---------while loop factorail-------------#
