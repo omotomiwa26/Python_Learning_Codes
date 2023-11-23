7f12d0be66b909838c18ab8a5d98e13861126ef7
omotomiwa26 <omotomiwa26@gmail.com>
2023-11-23 05:07:04 +0100

    fruit count

--- loops_dictionary.py
+++ loops_dictionary.py
@@ -19,3 +19,25 @@ cast = {
        }
 for keys, values in cast.items():
     print(F"Actor{keys}    Role{values}")
+
+
+#------------dictionary fruit count------------#
+fruit_count, not_fruit_count = 0, 0
+basket_items = {'apples': 4, 'oranges': 19, 'kites': 3, 'sandwiches': 8}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+#Iterate through the dictionary
+
+#if the key is in the list of fruits, add to fruit_count.
+for keys,values in basket_items.items():
+    if keys in fruits:
+        fruit_count += values
+
+#if the key is not in the list, then add to the not_fruit_count
+for keys,values in basket_items.items():
+    if keys not in fruits:
+        not_fruit_count += values
+
+
+
+print(fruit_count, not_fruit_count)
