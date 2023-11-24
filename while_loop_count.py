6863ce9c47845d35fdac9d5e77e8e359deee15ec
omotomiwa26 <omotomiwa26@gmail.com>
2023-11-24 01:53:23 +0100

    count with while loop

---
+++ while_loop_count.py
@@ -0,0 +1,11 @@
+#-----------while count----------------#
+start_num = 5
+end_num = 100
+count_by = 2
+
+# write a while loop that uses break_num as the ongoing number to 
+#   check against end_num
+break_num = start_num
+while break_num <= end_num:
+    break_num += count_by
+print(break_num)
