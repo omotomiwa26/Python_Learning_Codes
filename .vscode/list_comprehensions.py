1d30de995fdb94941defae1b57260c20d740a333
omotomiwa26 <omotomiwa26@gmail.com>
2023-11-29 02:02:17 +0100

    multiples of 3

--- .vscode/list_comprehensions.py
+++ .vscode/list_comprehensions.py
@@ -4,4 +4,6 @@ names = ["Rick Sanchez", "Morty Smith", "Summer Smith", "Jerry Smith", "Beth Smi
 first_names = [name.split()[0].lower() for name in names]
 print(first_names)
 
-#-------------multi
+#-------------Multiples Of Three---------#
+multiples_of_3 = [x*1 for x in range(3, 61, 3)] 
+print(multiples_of_3)
