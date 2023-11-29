3494d4bf24455608498e76b3dd8bf833fc733a4e
omotomiwa26 <omotomiwa26@gmail.com>
2023-11-29 01:53:58 +0100

    extract first names

---
+++ .vscode/list_comprehensions.py
@@ -0,0 +1,7 @@
+#----------Extract First Names------------#
+names = ["Rick Sanchez", "Morty Smith", "Summer Smith", "Jerry Smith", "Beth Smith"]
+
+first_names = [name.split()[0].lower() for name in names]
+print(first_names)
+
+#-------------multi
