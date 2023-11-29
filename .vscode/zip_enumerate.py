6839e9811825b481dd76f7fcec20959fbc5e5ca3
omotomiwa26 <omotomiwa26@gmail.com>
2023-11-29 01:33:44 +0100

    transpose zip

--- .vscode/zip_enumerate.py
+++ .vscode/zip_enumerate.py
@@ -28,3 +28,9 @@ names, heights = zip(*cast)
 
 print(names)
 print(heights)
+
+#-------------Transpose Zip------#
+data = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (9, 10, 11))
+
+data_transpose = tuple(zip(*data))
+print(data_transpose)
