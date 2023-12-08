92a24ab1291ff15ec420b0c9660f98fc5c82d233
omotomiwa26 <omotomiwa26@gmail.com>
2023-12-08 04:38:10 +0100

    readable timedelta

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-#-------------------population density function--------------
+#-------------------population density function--------------#
 # write your function here
 def population_density(population, land_area):
     total_population = population / land_area
@@ -12,3 +12,13 @@ print("expected result: {}, actual result: {}".format(expected_result1, test1))
 test2 = population_density(864816, 121.4)
 expected_result2 = 7123.6902801
 print("expected result: {}, actual result: {}".format(expected_result2, test2))
+
+#-----------readable timedelta------------------------#
+# write your function here
+def readable_timedelta(days):
+    nos_of_weeks = days // 7
+    nos_of_days = days % 7
+    return F"{nos_of_weeks} week(s) and {nos_of_days} day(s)."
+
+# test your function
+print(readable_timedelta(5094))
