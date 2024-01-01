bb4f280c078bd162d3cf640a2aa7a77495861d8a
omotomiwa26 <omotomiwa26@gmail.com>
2024-01-01 02:55:54 +0100

    lambda with map

---
+++ lambda.py
@@ -0,0 +1,16 @@
+#--------------lambda with map---------------#
+numbers = [
+              [34, 63, 88, 71, 29],
+              [90, 78, 51, 27, 45],
+              [63, 37, 85, 46, 22],
+              [51, 22, 34, 11, 18]
+           ]
+
+def mean(num_list):
+    return sum(num_list) / len(num_list)
+
+averages = list(map(mean, numbers))
+print(averages)
+
+averages2 = list(map(lambda num: sum(num) / len(num), numbers))
+print (averages2)
