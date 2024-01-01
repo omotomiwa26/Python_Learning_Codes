029ca31300f1d84e0abc40f7fdacc27c1140cca3
omotomiwa26 <omotomiwa26@gmail.com>
2024-01-01 03:51:52 +0100

    test script

---
+++ script.py
@@ -0,0 +1,14 @@
+how_many_snakes = 10
+snake_string = """
+Welcome to Python3!
+
+             ____
+            / . .\\
+            \  ---<
+             \  /
+   __________/ /
+-=:___________/
+
+<3, Juno
+"""
+print(snake_string * how_many_snakes)
